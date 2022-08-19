2645f631bce033e6198c2f9c62913c03c1e47ccf
Joe Heck <heckj@mac.com>
2022-08-18 17:29:39 -0700

    wip

--- Sources/yrs-swift-experiment/yrs_swift_experiment.swift
+++ Sources/yrs-swift-experiment/yrs_swift_experiment.swift
@@ -1,8 +1,19 @@
-import YrsC
+@_implementationOnly import YrsC
+
+// ref: https://developer.apple.com/documentation/swift/using-imported-c-functions-in-swift
+// example: https://github.com/jpsim/Yams
+// - https://github.com/jpsim/Yams/blob/main/Sources/Yams/Parser.swift
+// - https://github.com/jpsim/Yams/blob/main/Sources/Yams/YamlError.swift
+
 
 public struct yrs_swift_experiment {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        if let x: UnsafeMutablePointer<YDoc> = ydoc_new() {
+            // do stuff here
+            
+            ydoc_destroy(x)
+        }
     }
 }
